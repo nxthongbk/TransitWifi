a1dd4ae8771577e44e3be16608f79a67833aac1b
Thong Nguyen <nxthongbk@gmail.com>
2019-05-23 12:59:46 +0700

    add

---
+++ wifiApTestComponent/inc-gen/interfaces.h
@@ -0,0 +1,20 @@
+/*
+ * AUTO-GENERATED interface.h for the wifiApTestComponent component.
+
+ * Don't bother hand-editing this file.
+ */
+
+#ifndef __wifiApTestComponent_COMPONENT_INTERFACE_H_INCLUDE_GUARD
+#define __wifiApTestComponent_COMPONENT_INTERFACE_H_INCLUDE_GUARD
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "le_wifiAp_interface.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __wifiApTestComponent_COMPONENT_INTERFACE_H_INCLUDE_GUARD
